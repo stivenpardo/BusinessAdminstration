3e422bce42342632d3244cface5824eacfe70cf5
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-03-24 19:45:05 -0500

    Configuration for Settings in WebApi

---
+++ BusinessAdministration.Aplication.Core/PeopleManagement/Configuration/PeopleManagementConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using BusinessAdministration.Infrastructure.Data.Persistence.Core.Base.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessAdministration.Aplication.Core.PeopleManagement.Configuration
+{
+    public static class PeopleManagementConfigurator
+    {
+        /// <summary>
+        /// it can configure and use the microservice of people management 
+        ///<param name="services">Service collector that has the application catalog</param>
+        /// </summary>
+        public static void ConfigurePeopleManagementService(this IServiceCollection services, DbSettings settings)
+        {
+            //services.ConfigureMapper();
+            services.ConfigureBaseRepository(settings);
+        }
+    }
+}

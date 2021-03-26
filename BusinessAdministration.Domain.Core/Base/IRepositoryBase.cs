40fee0e48688e1891405a207092b277be4358808
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-03-25 19:05:14 -0500

    Add T for method generic

--- BusinessAdministration.Domain.Core/Base/IRepositoryBase.cs
+++ BusinessAdministration.Domain.Core/Base/IRepositoryBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -11,8 +12,8 @@ namespace BusinessAdministration.Domain.Core.Base
         public Task<T> Insert<T>(T entity) where T : EntityBase;
         public bool Update<T>(T entity) where T : EntityBase;
         public bool Delete<T>(T entity) where T : EntityBase;
-        public IEnumerable SearchMatching<T>(Expression<Func<T, bool>> predicate) where T : EntityBase;
+        public IEnumerable<T> SearchMatching<T>(Expression<Func<T, bool>> predicate) where T : EntityBase;
         public T SearchMatchingOneResult<T>(Expression<Func<T, bool>> predicate) where T : EntityBase;
-        public IEnumerable GetAll<T>() where T : EntityBase;
+        public IEnumerable<T> GetAll<T>() where T : EntityBase;
     }
 }
